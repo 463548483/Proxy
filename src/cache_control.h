583e4ec2dbbccd7ed2069e494bac8cee10899647
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-02-18 20:24:55 -0500

    fix minor error

--- src/cache_control.h
+++ src/cache_control.h
@@ -14,12 +14,12 @@ typedef struct RspCacheControl_type {
   bool no_store; //default: false; if true, do not store in cache
   bool no_cache; //default: false; if true, need revalidate everytime
   bool is_private; //default: false; if true, do not store in cache
-  int64_t max-age; // If does not have max-age, max-age=negative
+  int64_t max_age; // If does not have max-age, max-age=negative
 
   // Other field related to Cache
-  struct tm Expires; // If does not have Expires field or the field is invalid, Expires=NULL
-  uint64_t Age; // If does not have a valid Age field=>Age=0
-  std::string Etag; // default empty string
-  std::string Last-Modified; // default empty string
+  struct tm expires; // If does not have Expires field or the field is invalid, Expires=NULL
+  uint64_t age; // If does not have a valid Age field=>Age=0
+  std::string etag; // default empty string
+  std::string last_modified; // default empty string
 } RspCacheControl;
 #endif
