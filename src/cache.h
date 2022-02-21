24357157c2db8fcec5673687529a214433391a0d
zw255 <zw255@duke.edu>
2022-02-20 20:13:01 -0500

    add revalid in cache

--- src/cache.h
+++ src/cache.h
@@ -36,10 +36,12 @@ class Cache{
   time_t parse_time(RspCacheControl cache);
   public:
   Cache();
-  string search_record(string uri);
+  HttpResponse * search_record(string uri);
   bool check_time_valid(string uri);//check if fresh/expire
   bool check_tag_valid(string uri);//check if has Etag/last_modify
-  void store_record(string uri,HttpResponse);//store
+  void store_record(string uri,HttpResponse);//store single record
+  void replace_header(string uri,HttpResponse);//only replace header file
+  void replace_response(string uri,HttpResponse);//replace the whole response
 };
 
 
